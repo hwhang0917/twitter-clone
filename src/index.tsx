a297375b0c81ae2e6ef982bfec6da57bdd1f2949
Heesang whang <hwhang0917@gmail.com>
2020-11-02 02:36:41 +0900

    ➕ Add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import GlobalStyle from "./Styles/GlobalStyle";
+import firebase from "./firebase";
 
 const root = document.getElementById("root") as HTMLDivElement;
+
+console.log(firebase);
 
 ReactDOM.render(
   <>
